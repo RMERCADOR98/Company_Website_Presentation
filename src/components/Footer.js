7874ef7698788313f4d674a7a742cddafc7c5e11
RMERCADOR98 <51795747+RMERCADOR98@users.noreply.github.com>
2021-01-02 13:11:29 +0000

    presentation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,39 @@
 import React from "react";
+//Styling
+import "../Home.css";
 
 const Footer = () => {
   return (
-    <div style={{ padding: "5rem 20rem 5rem 20rem" }}>
+    <div>
+      <div style={{ padding: "5rem 20rem 2rem 20rem" }}>
+        <div
+          className="line"
+          style={{ height: "0.1rem", background: "#ABABAB" }}
+        />
+        <div
+          className="links"
+          class="d-flex justify-content-evenly"
+          style={{ marginTop: "3rem", color: "#ABABAB" }}
+        >
+          <a href="#we" style={{ textDecoration: "none", color: "#ABABAB" }}>
+            <h6>Who are we?</h6>
+          </a>
+          <a href="#smart" style={{ textDecoration: "none", color: "#ABABAB" }}>
+            <h6>Smart Cities</h6>
+          </a>
+          <a
+            href="#mission"
+            style={{ textDecoration: "none", color: "#ABABAB" }}
+          >
+            <h6>A nossa missão</h6>
+          </a>
+        </div>
+      </div>
       <div
-        className="line"
-        style={{ height: "0.1rem", background: "#ABABAB" }}
-      />
-      <div
-        className="links"
-        class="d-flex justify-content-evenly"
-        style={{ marginTop: "3rem", color: "#ABABAB" }}
+        class="d-flex justify-content-center"
+        style={{ paddingBottom: "1rem", color: "#d1d1d1" }}
       >
-        <a href="#we" style={{ textDecoration: "none", color: "#ABABAB" }}>
-          <h6>Who are we?</h6>
-        </a>
-        <a href="#smart" style={{ textDecoration: "none", color: "#ABABAB" }}>
-          <h6>Smart Cities</h6>
-        </a>
-        <a href="#mission" style={{ textDecoration: "none", color: "#ABABAB" }}>
-          <h6>A nossa missão</h6>
-        </a>
+        All Rights Reserved 2020
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,14 @@ import React from "react";
 import Banner from "../img/Banner.jpg";
 import we from "../img/we.jpg";
 import smart from "../img/smart.jpg";
+import logoBranco from "../img/logo_ranco.png";
+import logoPretoCores from "../img/logoPretoCores.svg";
 //Icons
 import { BiDownArrowAlt } from "react-icons/bi";
 //Components
 import Footer from "../components/Footer";
+//Styling
+import "../Home.css";
 
 const Home = () => {
   return (
@@ -16,6 +20,21 @@ const Home = () => {
           class="d-flex justify-content-center align-items-center"
           style={{ height: "100vh" }}
         >
+          <img
+            src={logoBranco}
+            alt=""
+            style={{
+              position: "absolute",
+              color: "white",
+              textDecoration: "none",
+              marginLeft: "0.5rem",
+              top: "0",
+              left: "0",
+              height: "5rem",
+              margin: "2rem",
+            }}
+          />
+
           <h1
             style={{
               position: "absolute",
@@ -26,14 +45,18 @@ const Home = () => {
           >
             Future now
           </h1>
-          <BiDownArrowAlt
+          <a
             style={{
               position: "absolute",
               color: "white",
               marginTop: "55rem",
               fontSize: "3rem",
             }}
-          />
+            href="#we"
+            alt=""
+          >
+            <BiDownArrowAlt />
+          </a>
           <img
             src={Banner}
             alt=""
@@ -55,18 +78,21 @@ const Home = () => {
         <div
           id="we"
           class="d-flex justify-content-evenly"
-          style={{ padding: "10rem 15rem" }}
+          style={{ padding: "10rem 20rem" }}
         >
-          <div style={{ width: "50%", marginRight: "1rem" }}>
+          <div
+            style={{ width: "50%", marginRight: "3rem", textAlign: "justify" }}
+          >
             <h1>Quem Somos?</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatum dolorum fugit necessitatibus veritatis ex rem quas
-              temporibus dicta cupiditate hic itaque, ad assumenda beatae ab
-              sequi sint facilis error ipsa!
+              A Flow Technologies é uma equipa de 2 makers nova e ambiciosa que
+              quer deixar a sua marca no mundo através das tecnologias.
+              <br />
+              Somos uma equipa curiosa, viemos de caminhos diferentes, com o
+              mesmo foco para tornar a nossa sociedade mais conectada e segura.
             </p>
           </div>
-          <div style={{ width: "50%", marginLeft: "1rem", height: "30vh" }}>
+          <div style={{ width: "50%", marginLeft: "3rem", height: "30vh" }}>
             <img
               src={we}
               alt=""
@@ -87,22 +113,33 @@ const Home = () => {
         <div
           id="smart"
           class="d-flex justify-content-evenly"
-          style={{ padding: "10rem 15rem" }}
+          style={{ padding: "10rem 20rem" }}
         >
-          <div style={{ width: "50%", marginRight: "1rem" }}>
+          <div style={{ width: "50%", marginRight: "3rem" }}>
             <img
               src={smart}
               alt=""
               style={{ width: "100%", height: "40vh", objectFit: "cover" }}
             />
           </div>
-          <div style={{ width: "50%", marginLeft: "1rem" }}>
+          <div
+            style={{ width: "50%", marginLeft: "3rem", textAlign: "justify" }}
+          >
             <h1>Smart Cities</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatum dolorum fugit necessitatibus veritatis ex rem quas
-              temporibus dicta cupiditate hic itaque, ad assumenda beatae ab
-              sequi sint facilis error ipsa!
+              Uma Smart City usa tecnologia digital para conectar , proteger e
+              auxiliar a vida das pessoas. Sensores IOT, cameras de video
+              vigilancia, redes sociais e outras aspectos agem com um sistema
+              unico, fornecendo à cidade e aos seus habitantes uma resposta
+              constante, para que se mantenham informados.
+              <br /> Alguns temas abordados:
+              <br />
+              <br /> &nbsp;&nbsp; &nbsp; &nbsp; - Energia
+              <br /> &nbsp;&nbsp; &nbsp; &nbsp; - Tecnologia e Inovação
+              <br /> &nbsp;&nbsp; &nbsp; &nbsp; - Sustentabilidade
+              <br /> &nbsp;&nbsp; &nbsp; &nbsp; - Planeamento Urbano
+              <br /> &nbsp;&nbsp; &nbsp; &nbsp; - Mobilidade
+              <br /> &nbsp;&nbsp; &nbsp; &nbsp; - Segurança
             </p>
           </div>
         </div>
@@ -117,20 +154,25 @@ const Home = () => {
         <div
           id="mission"
           class="d-flex justify-content-evenly"
-          style={{ padding: "10rem 15rem" }}
+          style={{ padding: "10rem 20rem", textAlign: "justify" }}
         >
-          <div style={{ width: "50%", marginRight: "1rem" }}>
-            <h1>A nossa missão</h1>
+          <div style={{ width: "50%", marginRight: "3rem" }}>
+            <h1>A nossa visão</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatum dolorum fugit necessitatibus veritatis ex rem quas
-              temporibus dicta cupiditate hic itaque, ad assumenda beatae ab
-              sequi sint facilis error ipsa!
+              Criar soluções inovadoras que melhorem o estilo de vida de todos é
+              a nossa visão! Acreditamos que o futuro tecnológico tem de ser
+              desenvolvido tendo em conta as necessidades dos mais diversos
+              serviços que constituem uma cidade para o bem estar dos
+              habitantes. Buscamos o progresso e acessibilidade para todas as
+              pessoas, com isto em mente, olhamos para o futuro com positividade
+              e esperança de que a segurança deva ser algo garantido e sem
+              receios de utilizar novas tecnologias.
             </p>
           </div>
-          <div style={{ width: "50%", marginLeft: "1rem", height: "30vh" }}>
+          <div style={{ width: "50%", marginLeft: "3rem", height: "30vh" }}>
             <img
-              src={we}
+              id="logo"
+              src={logoPretoCores}
               alt=""
               style={{ width: "100%", height: "40vh", objectFit: "cover" }}
             />
